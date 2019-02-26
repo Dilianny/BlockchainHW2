e1d70ac9b538e671d675d982acf518f5536f5f73
Dilianny <dilannyevans@gmail.com>
2019-02-25 20:57:05 -0500

    Update sha1hash.py

--- sha1hash.py
+++ sha1hash.py
@@ -6,7 +6,6 @@ if len(sys.argv)<2:
 else:
     hash = sys.argv[1]
     tries=0
-    #salt="f0744d60dd500c92c0d37c16174cc58d3c4bdd8e"
     salted=""
     start = time.clock()
     with open("10-million-password-list-top-1000000.txt") as passwords:
@@ -23,18 +22,22 @@ else:
                     print "password is: ", guess
                     break
             elif (len(sys.argv)==3):
+                start = time.clock()
                 if hashlib.sha1(bytes(guess)).hexdigest() == sys.argv[2]:
-                    salted = guess
+                    salted = guess.strip()
                     print "salt text is: ", salted
-                for passw in passwords.readlines():
-                    passw= passw.strip()
-                    saltedp = salted + passw
-                    print saltedp
-                    if hashlib.sha1(bytes(saltedp)).hexdigest() == hash:
-                        end =time.clock()
-                        print "number of tries: ", tries
-                        print "time taken: ", end-start, "seconds."
-                        print "password is: ", passw
-                        break
-                    else:
-                        print "Password not found"
+                    with open("10-million-password-list-top-1000000.txt") as passwords:
+                        for guess in passwords.readlines():
+                            guess= guess.strip()
+                            saltedp = salted + guess
+                            tries=tries+1
+                            if hashlib.sha1(bytes(saltedp)).hexdigest() != sys.argv[1]:
+                                continue
+                            elif hashlib.sha1(bytes(saltedp)).hexdigest() == sys.argv[1]:
+                                end =time.clock()
+                                print "number of tries: ", tries
+                                print "time taken: ", end-start, "seconds."
+                                print "password is: ", guess
+                                break
+                            #else:
+                                #print "Password not found"
